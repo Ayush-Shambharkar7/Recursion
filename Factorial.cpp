32e848f1836b7c85bb53445c8b4000c75bff6908
Ayush-Shambharkar7 <ayushshambharkar0007@gmail.com>
2024-09-30 20:21:22 +0530

    Create Factorial.cpp

---
+++ Factorial.cpp
@@ -0,0 +1,28 @@
+//Ayush Shambharkar
+//23070123034
+//Aim:-factorial using recusrion
+
+#include<iostream>
+using namespace std;
+
+int fact(int n)
+{
+    if (n<=1)
+        return 1;
+
+    else return (n*fact(n-1));
+}
+int main()
+{
+    int a;
+    cout<<"Enter an integer: "<<endl;
+    cin>>a;
+    cout<<"Factorial is: "<<fact(a);
+}
+
+/*
+OUTOUT: 
+Enter an integer: 
+5
+Factorial is: 120
+*/
